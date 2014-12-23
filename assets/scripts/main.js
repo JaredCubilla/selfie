22d7bc388105728aff769933d55cfbdec5e4a0df
JaredCubilla <deraj.allibuc@gmail.com>
2014-12-23 12:47:28 -0500

    removed node_modules from git, pretty button

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,12 @@
 console.log('test');
+
+
+$('#capture-button').hover(function() {
+	'use strict';
+	$(this).addClass('hover');
+	$('#capture-button .fa').addClass('hover');
+}, function() {
+	'use strict';
+	$(this).removeClass('hover');
+	$('#capture-button .fa').removeClass('hover');
+});
