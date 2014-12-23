4657ee025c7e794cf95afa71802938e25e6dbadf
JaredCubilla <deraj.allibuc@gmail.com>
2014-12-23 14:01:46 -0500

    anonymous function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,21 @@
-console.log('test');
+(function() {
+	'use strict';
+
+	function takePicture() {
+		console.log('Picture taken!');
+	}
 
 
-$('#capture-button').hover(function() {
-	'use strict';
-	$(this).addClass('hover');
-	$('#capture-button .fa').addClass('hover');
-}, function() {
-	'use strict';
-	$(this).removeClass('hover');
-	$('#capture-button .fa').removeClass('hover');
-});
+	$('#capture-button').hover(function() {
+		$(this).addClass('hover');
+		$('#capture-button .fa').addClass('hover');
+	}, function() {
+		$(this).removeClass('hover');
+		$('#capture-button .fa').removeClass('hover');
+	});
+
+	$('#capture-button').click(function() {
+		takePicture();
+	});
+
+})();
