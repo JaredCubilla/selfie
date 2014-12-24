ed37a190f13a9547a30aae79e662f693eadd275c
JaredCubilla <deraj.allibuc@gmail.com>
2014-12-24 13:10:12 -0500

    basic features implemented

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,55 @@
 (function() {
 	'use strict';
 
+
+	var streaming = false,
+		video = document.querySelector('#video'),
+		width = 500,
+		height = 0,
+		ctx = document.querySelector('#canvas').getContext('2d');
+
+	navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+	navigator.getMedia({video: true, audio: false}, function(stream) {
+		if (navigator.mozGetUserMedia) {
+			$('#video').mozSrcObject = stream;
+		} else {
+			var url = window.URL || window.webkitURL;
+			$('#video').attr('src', url.createObjectURL(stream));
+		}
+
+		video.play();
+	}, function(error) {
+		console.log('Dun dun dun! ' + error);
+	});
+
+	video.addEventListener('canplay', function(e) {
+		if (!streaming) {
+			height = video.videoHeight / (video.videoWidth/width);
+
+			$('#video').attr({
+				width: width,
+				height: height
+			});
+
+			$('#canvas').attr({
+				width: width,
+				height: height
+			});
+
+			ctx.translate(width, 0);
+			ctx.scale(-1, 1);
+
+			streaming = true;
+		}
+	}, false);
+
 	function takePicture() {
 		console.log('Picture taken!');
-	}
 
+		ctx.drawImage(video, 0, 0, width, height);
+		$('#image').attr('src', canvas.toDataURL('image/png'));
+	}
 
 	$('#capture-button').hover(function() {
 		$(this).addClass('hover');
@@ -17,5 +62,9 @@
 	$('#capture-button').click(function() {
 		takePicture();
 	});
+
+	if (!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia)) {
+		console.log('User media not supported.');
+	}
 
 })();
